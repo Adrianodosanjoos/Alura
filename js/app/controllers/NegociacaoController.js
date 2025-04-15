395ac35e0f801e4f5f62c1446b350b94f78ea432
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-04-15 14:41:55 -0300

    .

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -7,15 +7,18 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
-        this._negociacoesView = new NegociacaoView($('#negociacoesView'));
 
-        this._negociacoesView.update();
+
+        this._negociacoesView = new NegociacaoView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
     }
         
     adiciona(event) {
+
         event.preventDefault();
         
          this._listaNegociacoes.adiciona(this._criaNegociacao());
+         this._negociacoesView.update(this._listaNegociacoes);
          this._limpaFormulario();
     }
 
