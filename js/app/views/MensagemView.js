6cf1abb8fd0df782eaafabc9f7fa67b0baac7c9a
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-04-21 13:28:26 -0300

    Atualização

--- js/app/views/MensagemView.js
+++ js/app/views/MensagemView.js
@@ -1,7 +1,7 @@
-class MensagemView {
+class MensagemView extends View {
 
     constructor(elemento) {
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model) {
@@ -10,8 +10,4 @@ class MensagemView {
 
     }
 
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
-    }
 }
--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -1,8 +1,8 @@
 
-class NegociacaoView {
+class NegociacaoView extends View {
 
     constructor(elemento) {
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model) {
@@ -39,12 +39,6 @@ class NegociacaoView {
     </table>
    
         `;
-    }
-
-    update(model) {
-        
-       this._elemento.innerHTML = this._template(model);
-
     }
 
 }
